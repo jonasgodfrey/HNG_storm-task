8e02e51ce3a0b01d34c506ae6efff75e4b762901
Godfrey Jonas <jonasgodfrey@users.noreply.github.com>
2020-06-06 06:43:00 +0100

    Commit
    pull my script

---
+++ testScripts/Enangha.js
@@ -0,0 +1,4 @@
+const pull_out =() => {
+    return "Hello World, this is [Godfrey] [Jonas] with HNGi7 ID [HNG-04007] using [Javascript] for stage 2 task.hello@jonasgodfrey.com"
+}
+console.log(pull_out());
